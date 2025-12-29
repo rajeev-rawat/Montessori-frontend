be8920f1b4edeb64919d566e6336adcbb98aea1e
Rajeev Rawat <rajeevrawat@Rajeevs-MacBook-Air.local>
2025-12-29 10:39:10 +0530

    merge code

---
+++ .next/dev/static/chunks/Desktop_MONTESSORI_Montessori-frontend_app_layout_tsx_50511a5c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__c9af222c._.css",
+  "static/chunks/012c2_1710f10c._.js"
+],
+    source: "dynamic"
+});
---
+++ .next/dev/static/chunks/Desktop_MONTESSORI_Montessori-frontend_app_page_tsx_19666d82._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/Desktop_MONTESSORI_Montessori-frontend_21ff694e._.js",
+  "static/chunks/012c2_8c5ddfd8._.js"
+],
+    source: "dynamic"
+});
