8d437268bc89a479c4367e774792327d6f4d51bf
Rajeev Rawat <rajeevrawat@192.168.1.12>
2026-01-11 14:28:27 +0530

    issue

--- .next/dev/static/chunks/Desktop_MONTESSORI_Montessori-frontend_app_page_tsx_09048c5e._.js
+++ .next/dev/static/chunks/Desktop_MONTESSORI_Montessori-frontend_app_page_tsx_09048c5e._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/Desktop_MONTESSORI_Montessori-frontend_a1d6b481._.js",
-  "static/chunks/012c2_1f88180a._.js"
+  "static/chunks/Desktop_MONTESSORI_Montessori-frontend_20fc3296._.js",
+  "static/chunks/012c2_94c3f478._.js"
 ],
     source: "dynamic"
 });
