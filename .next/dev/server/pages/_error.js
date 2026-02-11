f0c9b84a5e08186764d1cbfbb05af3967b562f17
Rajeev Rawat <rajeevrawat@email.com>
2026-02-11 22:28:17 +0530

    rajeev-changes

---
+++ .next/dev/server/pages/_error.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
+R.c("server/chunks/ssr/012c2_11235f49._.js")
+R.c("server/chunks/ssr/[externals]_next_dist_shared_lib_no-fallback-error_external_59b92b38.js")
+R.c("server/chunks/ssr/012c2_7e4448c9._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__e6a4d965._.js")
+R.c("server/chunks/ssr/012c2_next_e9d6e7b7._.js")
+R.m("[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
+module.exports=R.m("[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/Desktop/MONTESSORI/Montessori-frontend/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
